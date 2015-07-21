4fbfd6517c9ab4da9d43ff75acc4df8075bda53a
DVRPCfreight <mruane@dvrpc.org>
2015-07-21 18:20:04 -0400

    Commit Version 3.1.2
    Full commit of most recent published app version.

---
+++ data/FCmajorPts.js
@@ -0,0 +1,31 @@
+{
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 2, "Id": 2, "County": "Bucks", "Center_Typ": "Major", "Name": "Warminster \/ Northampton", "Township_s": "Warminster Township, Northamption Township, Warwick Township, Ivyland Borough", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC2.pdf target=_blank>Detailed Profile<\/a>", "Acres": 957.402192 }, "geometry": { "type": "Point", "coordinates": [ -75.081623, 40.205183 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 3, "Id": 3, "County": "Bucks", "Center_Typ": "Major", "Name": "Bensalem \/ Street Road", "Township_s": "Bensalem Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC3.pdf target=_blank>Detailed Profile<\/a>", "Acres": 735.657178 }, "geometry": { "type": "Point", "coordinates": [ -74.933363, 40.079352 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 7, "Id": 7, "County": "Bucks", "Center_Typ": "Major", "Name": "Falls \/ Penn-Warner", "Township_s": "Falls Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC7.pdf target=_blank>Detailed Profile<\/a>", "Acres": 773.424952 }, "geometry": { "type": "Point", "coordinates": [ -74.790736, 40.189819 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 16, "Id": 16, "County": "Philadelphia", "Center_Typ": "Major", "Name": "South Philadelphia \/ Packer Ave.", "Township_s": "Philadelphia City", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC16.pdf target=_blank>Detailed Profile<\/a>", "Acres": 1293.917381 }, "geometry": { "type": "Point", "coordinates": [ -75.144221, 39.904294 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 19, "Id": 19, "County": "Gloucester", "Center_Typ": "Major", "Name": "West Deptford \/ Mantua Grove Road", "Township_s": "West Deptford Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC19.pdf target=_blank>Detailed Profile<\/a>", "Acres": 930.936673 }, "geometry": { "type": "Point", "coordinates": [ -75.209129, 39.830846 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 20, "Id": 20, "County": "Gloucester", "Center_Typ": "Major", "Name": "Paulsboro \/ Greenwich", "Township_s": "Greenwich Township,  West Deptford Township, Paulsboro Borough", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC20.pdf target=_blank>Detailed Profile<\/a>", "Acres": 1561.230929 }, "geometry": { "type": "Point", "coordinates": [ -75.259104, 39.840863 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 24, "Id": 24, "County": "Chester", "Center_Typ": "Major", "Name": "Downingtown \/ Boot Road", "Township_s": "East Caln Township, West Whiteland Township, Downingtown Borough", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC24.pdf target=_blank>Detailed Profile<\/a>", "Acres": 788.540008 }, "geometry": { "type": "Point", "coordinates": [ -75.669125, 40.009246 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 27, "Id": 27, "County": "Montgomery", "Center_Typ": "Major", "Name": "Pottstown \/ Limerick", "Township_s": "Lower Pottsgrove Township, Limerick Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC27.pdf target=_blank>Detailed Profile<\/a>", "Acres": 702.449605 }, "geometry": { "type": "Point", "coordinates": [ -75.632973, 40.242126 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 30, "Id": 30, "County": "Montgomery", "Center_Typ": "Major", "Name": "Towamencin \/ Hatfield", "Township_s": "Lower Salford Township, Towamencin Township, Hatfield Township, Franconia Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC30.pdf target=_blank>Detailed Profile<\/a>", "Acres": 456.933058 }, "geometry": { "type": "Point", "coordinates": [ -75.356128, 40.266262 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 31, "Id": 31, "County": "Mercer", "Center_Typ": "Major", "Name": "Robbinsville \/ NJ Turnpike Interchange 7A", "Township_s": "Robbinsville Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC31.pdf target=_blank>Detailed Profile<\/a>", "Acres": 918.428241 }, "geometry": { "type": "Point", "coordinates": [ -74.587154, 40.196090 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 37, "Id": 37, "County": "Camden", "Center_Typ": "Major", "Name": "Camden \/ Gloucester City Waterfront", "Township_s": "Camden City, Gloucester City", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC37.pdf target=_blank>Detailed Profile<\/a>", "Acres": 734.879314 }, "geometry": { "type": "Point", "coordinates": [ -75.125685, 39.922526 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 39, "Id": 39, "County": "Camden", "Center_Typ": "Major", "Name": "Pennsauken \/ NJ 90", "Township_s": "Pennsauken Township", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC39.pdf target=_blank>Detailed Profile<\/a>", "Acres": 861.283851 }, "geometry": { "type": "Point", "coordinates": [ -75.036365, 39.983563 ] } }
+,
+{ "type": "Feature", "properties": { "OBJECTID": 40, "Id": 40, "County": "Montgomery", "Center_Typ": "Major", "Name": "Upper Gwynedd \/  West Point", "Township_s": "Upper Gwynedd Township, Lansdale Borough", "Act_1": "n\/a", "Source": "2012 DVRPC", "REPORT": "<a href=http:\/\/www.dvrpc.org\/webmaps\/PhillyFreightFinder\/reports\/FC\/FC40.pdf target=_blank>Detailed Profile<\/a>", "Acres": 670.327859 }, "geometry": { "type": "Point", "coordinates": [ -75.301955, 40.214874 ] } }
+
+]
+}
---
+++ data/valueRegion.json
@@ -0,0 +1,10 @@
+{
+  "region":[{"type":"Truck","value":340.3,"tons":267.6},
+{"type":"Rail","value":11.3,"tons":23.8},
+{"type":"Water","value":12.4,"tons":24.7},
+{"type":"Air/Air + Truck","value":2.3,"tons":1},
+{"type":"Multiple Modes","value":62.3,"tons":8.5},
+{"type":"Pipeline","value":14.1,"tons":33.5},
+{"type":"Other and Unknown","value":6.8,"tons":3.9}]
+}
+
