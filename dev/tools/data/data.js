60a4c9ffc8a2abffa037614b9df811f4655c4277
Michael Ruane <mruane@dvrpc.org>
2024-04-12 09:36:19 -0400

    fix: load error

--- dev/tools/data/data.js
+++ dev/tools/data/data.js
@@ -1,31 +1,3 @@
-const loadjs = (FILE_URL, async = true, type = "text/javascript") => {
-    return new Promise((resolve, reject) => {
-        try {
-            const scriptEle = document.createElement("script");
-            scriptEle.type = type;
-            scriptEle.async = async;
-            scriptEle.src = FILE_URL;
-
-            scriptEle.addEventListener("load", (ev) => {
-                resolve({
-                    status: true
-                });
-            });
-
-            scriptEle.addEventListener("error", (ev) => {
-                reject({
-                    status: false,
-                    message: `Failed to load the script ＄{FILE_URL}`
-                });
-            });
-
-            document.body.appendChild(scriptEle);
-        } catch (error) {
-            reject(error);
-        }
-    });
-};
-
 const headerOffset = 65
 
 const tocLinks = document.querySelectorAll('.tocLink');
